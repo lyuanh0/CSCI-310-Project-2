7e7e8d05592d3cfad0c4c1a5add8589b183efbcd
Devin Thorpe <devinthorpe77@gmail.com>
2025-10-29 11:08:46 -0700

    updated User class with all variables and getters and setters

--- app/src/main/java/com/example/chatpet/data/model/User.java
+++ app/src/main/java/com/example/chatpet/data/model/User.java
@@ -4,9 +4,11 @@ import java.util.Date;
 
 public class User {
     private String username;
+    private String email;
     private String password;
     private Pet currentPet;
     private Date birthday;
+    private String avatar;
 
     public User() {}
 
@@ -18,12 +20,18 @@ public class User {
     // Getters and Setters
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
+    public Date getBirthday() {return  birthday;}
+    public void setBirthday(Date birthday){this.birthday = birthday;}
+    public String getAvatar(){return avatar;}
+    public void setAvatar(String avatar){this.avatar = avatar;}
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 
     public Pet getCurrentPet() { return currentPet; }
     public void setCurrentPet(Pet pet) { this.currentPet = pet; }
+    public String getEmail() {return email;}
+    public void setEmail(String emaill){this.email = emaill;}
 
     public void choosePet(String petType, String petName) {
 
